cc004cd5bbbaeff749c670b849fde8bf84266607
Lekan <jimoridwanlekan@gmail.com>
2022-09-12 17:02:37 +0100

    Add rack-cors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,10 +1,10 @@
-class MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
   before_action :set_message, only: %i[ show update destroy ]
 
   # GET /messages
   def index
     @message = Message.order(Arel.sql('RANDOM()')).first
 
-    render json: @message.greeting
+    render json: { message: @message.greeting }
   end
 end
